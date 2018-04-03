1051340b9ddc70dd90f4c4c013d5741756cbba6b
tenx <ying1_zheng@ssqian.com.cn>
2018-04-03 19:35:53 +0800

    加入 SPRING BOOT TEST

--- hspaas/hssms/src/main/java/com/huashi/sms/HsSmsApplication.java
+++ hspaas/hssms/src/main/java/com/huashi/sms/HsSmsApplication.java
@@ -2,6 +2,7 @@ package com.huashi.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 import com.huashi.common.util.LogUtils;
@@ -14,7 +15,9 @@ import com.huashi.common.util.LogUtils;
 public class HsSmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HsSmsApplication.class, args);
+	    ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(HsSmsApplication.class, args);
+	    configurableApplicationContext.registerShutdownHook();
+	    
 		LogUtils.info("华时短信服务项目已启动");
 		
 //		try {
