4a553e353fb99df76fd38be22129c46f9ec1aa93
tenx <ying1_zheng@ssqian.com.cn>
2018-06-06 15:07:33 +0800

    加入大数据自定义HTTP接口

---
+++ hspaas/hs-developer/src/main/java/com/huashi/developer/api/SmsBdSendApi.java
@@ -0,0 +1,38 @@
+package com.huashi.developer.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class SmsBdSendApi extends BasicApiSupport{
+
+    public void addMode() {
+
+    }
+
+    public void updMode() {
+    }
+
+    public void qryMode() {
+
+    }
+
+    public void delMode() {
+
+    }
+    
+    public void qryOrderDtl() {
+
+    }
+    
+    public void smsSend() {
+
+    }
+    
+    public void callback() {
+
+    }
+    
+
+}
