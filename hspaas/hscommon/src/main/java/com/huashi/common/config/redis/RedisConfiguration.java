f184b83a195b9fd6045a61850cbb7d2b57a279fc
tenx <ying1_zheng@ssqian.com.cn>
2018-06-23 22:23:14 +0800

    REDIS改回原有的 集群方案

--- hspaas/hscommon/src/main/java/com/huashi/common/config/redis/RedisConfiguration.java
+++ hspaas/hscommon/src/main/java/com/huashi/common/config/redis/RedisConfiguration.java
@@ -1,6 +1,8 @@
 package com.huashi.common.config.redis;
 
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +14,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -20,118 +24,147 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import redis.clients.jedis.JedisPoolConfig;
 
+import com.alibaba.druid.util.StringUtils;
+
 /**
+ * TODO Redis配置
  * 
-  * TODO Redis配置
-  * 
-  * @author zhengying
-  * @version V1.0   
-  * @date 2018年1月31日 上午10:36:49
+ * @author zhengying
+ * @version V1.0
+ * @date 2018年1月31日 上午10:36:49
  */
 @Configuration
 @EnableCaching
 @Order(1)
 public class RedisConfiguration extends CachingConfigurerSupport {
 
-	@Value("${redis.host}")
-	private String host;
+    @Value("${redis.password}")
+    private String  password;
 
-	@Value("${redis.port}")
-	private int port;
+    @Value("${redis.pool.maxActive}")
+    private Integer maxTotal;
 
-	@Value("${redis.password}")
-	private String password;
+    @Value("${redis.pool.minIdle}")
+    private Integer minIdle;
 
-	@Value("${redis.pool.maxActive}")
-	private Integer maxTotal;
+    @Value("${redis.pool.maxIdle}")
+    private Integer maxIdle;
 
-	@Value("${redis.pool.minIdle}")
-	private Integer minIdle;
-	
-	@Value("${redis.pool.maxIdle}")
-	private Integer maxIdle;
+    @Value("${redis.pool.maxWaitMillis}")
+    private Integer maxWait;
 
-	@Value("${redis.pool.maxWaitMillis}")
-	private Integer maxWait;
+    @Value("${redis.client.connectionTimeout}")
+    private Integer timeout;
 
-	@Value("${redis.client.connectionTimeout}")
-	private Integer timeout;
+    @Value("${redis.database}")
+    private int     database;
 
-	@Value("${redis.database}")
-	private int database;
+    @Value("${redis.pool.testOnBorrow}")
+    private boolean testOnBorrow;
+    @Value("${redis.pool.testOnReturn}")
+    private boolean testOnReturn;
 
-	@Value("${redis.pool.testOnBorrow}")
-	private boolean testOnBorrow;
-	@Value("${redis.pool.testOnReturn}")
-	private boolean testOnReturn;
+    @Value("${redis.cluster.nodes}")
+    private String  clusterNodes;
 
-	@Bean
-	public JedisPoolConfig jedisPoolConfig() {
-		JedisPoolConfig config = new JedisPoolConfig();
-		config.setMaxTotal(maxTotal);
-		config.setMinIdle(minIdle);
-		config.setMaxIdle(maxIdle);
-		config.setMaxWaitMillis(timeout);
-		config.setTestOnBorrow(testOnBorrow);
-		config.setTestOnReturn(testOnReturn);
-		return config;
-	}
+    @Value("${redis.cluster.timeout}")
+    private Integer clusterTimeout;
 
-	@Bean(name = "jedisConnectionFactory")
-	public JedisConnectionFactory redisSmsConnectionFactory(JedisPoolConfig jedisPoolConfig) {
-		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(jedisPoolConfig);
-		jedisConnectionFactory.setHostName(host);
-		jedisConnectionFactory.setPort(port);
+    @Value("${redis.cluster.maxRedirects:5}")
+    private Integer clusterMaxRedirects;
+
+    @Bean
+    public JedisPoolConfig jedisPoolConfig() {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxTotal);
+        config.setMinIdle(minIdle);
+        config.setMaxIdle(maxIdle);
+        config.setMaxWaitMillis(timeout);
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(testOnReturn);
+        return config;
+    }
+
+    public Iterable<RedisNode> redisNodes() {
+        if (StringUtils.isEmpty(clusterNodes)) {
+            throw new IllegalArgumentException("Redis cluster nodes can not be null");
+        }
+
+        Set<RedisNode> redisNoes = new HashSet<>();
+        String[] nodes = clusterNodes.split(",");
+        for (String node : nodes) {
+            String[] ipPort = node.split(":");
+            redisNoes.add(new RedisNode(ipPort[0].trim(), Integer.valueOf(ipPort[1])));
+        }
+
+        return redisNoes;
+    }
+
+    @Bean
+    public RedisClusterConfiguration redisClusterConfiguration() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        redisClusterConfiguration.setClusterNodes(redisNodes());
+        redisClusterConfiguration.setMaxRedirects(clusterMaxRedirects);
+
+        return redisClusterConfiguration;
+    }
+
+    @Bean(name = "jedisConnectionFactory")
+	public JedisConnectionFactory redisSmsConnectionFactory(RedisClusterConfiguration redisClusterConfiguration,
+	                                                        JedisPoolConfig jedisPoolConfig) {
+		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(redisClusterConfiguration, jedisPoolConfig);
+//		jedisConnectionFactory.setHostName(host);
+//		jedisConnectionFactory.setPort(port);
 		jedisConnectionFactory.setPassword(password);
 		jedisConnectionFactory.setUsePool(true);
 		jedisConnectionFactory.setDatabase(database);
+		
 		return jedisConnectionFactory;
 	}
 
-	@Bean(name = "stringRedisTemplate")
-	public StringRedisTemplate stringRedisTemplate(
-			@Qualifier("jedisConnectionFactory") JedisConnectionFactory jedisConnectionFactory) {
-		StringRedisTemplate template = new StringRedisTemplate();
-		template.setConnectionFactory(jedisConnectionFactory);
-		return template;
+    @Bean(name = "stringRedisTemplate")
+    public StringRedisTemplate stringRedisTemplate(@Qualifier("jedisConnectionFactory") JedisConnectionFactory jedisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(jedisConnectionFactory);
+        return template;
 
-	}
+    }
 
-	@Bean
-	public KeyGenerator wiselyKeyGenerator() {
-		return new KeyGenerator() {
-			@Override
-			public Object generate(Object target, Method method, Object... params) {
-				StringBuilder sb = new StringBuilder();
-				sb.append(target.getClass().getName());
-				sb.append(method.getName());
-				for (Object obj : params) {
-					sb.append(obj.toString());
-				}
-				return sb.toString();
-			}
-		};
-	}
+    @Bean
+    public KeyGenerator wiselyKeyGenerator() {
+        return new KeyGenerator() {
 
-	@Bean
-	public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
-		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-		// Number of seconds before expiration. Defaults to unlimited (0)
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
 
-		// 设置key-value超时时间
-		cacheManager.setDefaultExpiration(10);
-		return cacheManager;
-	}
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        // Number of seconds before expiration. Defaults to unlimited (0)
 
-	@Bean(name = "redisTemplate")
-	public RedisTemplate<String, Object> redisTemplate(
-			@Qualifier("jedisConnectionFactory") RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(connectionFactory);
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new RedisObjectSerializer());
-		template.afterPropertiesSet();
-		return template;
-	}
+        // 设置key-value超时时间
+        cacheManager.setDefaultExpiration(10);
+        return cacheManager;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(@Qualifier("jedisConnectionFactory") RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new RedisObjectSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
 
 }
