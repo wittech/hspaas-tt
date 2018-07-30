2461dd37d078ca3b3d9a5cc6b0e82c004ff27a2a
tenx <ying1_zheng@ssqian.com.cn>
2018-07-30 11:16:28 +0800

    按照V1.8协议进行调整 JSON格式

--- hspaas/hs-developer/src/main/java/com/huashi/developer/config/WebServerConfiguration.java
+++ hspaas/hs-developer/src/main/java/com/huashi/developer/config/WebServerConfiguration.java
@@ -6,9 +6,8 @@ import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory
 import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class WebServerConfiguration {
 
 	@Bean
